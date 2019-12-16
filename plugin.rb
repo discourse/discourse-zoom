909534344e7c732efcc3adf69eaaf503cf83620a
Penar Musaraj <pmusaraj@gmail.com>
2019-12-16 13:53:52 -0500

    Add License and basic enabled setting

---
+++ plugin.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# name: discourse-zoom
+# about: Integrate Zoom events in Discourse.
+# version: 0.0.1
+# authors: Penar Musaraj, Roman Rizzi, Mark VanLandingham
+# url: https://github.com/discourse-org/discourse-zoom
+
+enabled_site_setting :zoom_enabled
