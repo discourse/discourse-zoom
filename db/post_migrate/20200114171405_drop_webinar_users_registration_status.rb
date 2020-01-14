30ee28a788994e01b0ea24f73d8b7151b9620d82
Mark VanLandingham <markvanlan@gmail.com>
2020-01-14 11:18:28 -0600

    Drop registration_status in post_migration

--- db/post_migrate/20200114171405_drop_webinar_users_registration_status.rb
+++ db/post_migrate/20200114171405_drop_webinar_users_registration_status.rb
@@ -1,5 +1,5 @@
 class DropWebinarUsersRegistrationStatus < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :webinar_users, :registration_status, :integer
+  def up
+    remove_column :webinar_users, :registration_status
   end
 end
