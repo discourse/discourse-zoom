ba210bd2a51f2004f47666ef4725abafdbb30bd8
Penar Musaraj <pmusaraj@gmail.com>
2020-03-03 18:45:55 -0500

    Fix URL, update README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # discourse-zoom
+
+See https://meta.discourse.org/t/zoom-webinars-plugin/142711 for details. 
--- plugin.rb
+++ plugin.rb
@@ -4,7 +4,7 @@
 # about: Integrate Zoom events in Discourse.
 # version: 0.0.1
 # authors: Penar Musaraj, Roman Rizzi, Mark VanLandingham
-# url: https://github.com/discourse-org/discourse-zoom
+# url: https://github.com/discourse/discourse-zoom
 
 enabled_site_setting :zoom_enabled
 register_asset "stylesheets/common/zoom.scss"
