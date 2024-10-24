49bb9577590332b1a4a5ea404e57d27f7d70314b
Kelvin Tan <kelv@discourse.org>
2024-10-24 18:07:51 +0800

    DEV: resolve Rails/ReversibleMigrationMethodDefinition errors

--- db/migrate/20200114171405_drop_webinar_users_registration_status.rb
+++ db/migrate/20200114171405_drop_webinar_users_registration_status.rb
@@ -3,4 +3,8 @@ class DropWebinarUsersRegistrationStatus < ActiveRecord::Migration[6.0]
   def up
     remove_column :webinar_users, :registration_status
   end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
 end
