62dffd4951a1766c6e626f345c6d97f15a0180ef
David Taylor <david@taylorhq.com>
2025-01-09 12:04:07 +0000

    FIX: Modernize topic page integration
    - `dec.connect` -> `RenderGlimmer` with gjs
    - `@observes` -> `bodyClass` helper
    
    This makes the plugin compatible with the colocation in 7a35b97442e31fc1db8230257933e682e92fad21
    
    Also adds a very simple system spec to make sure topic pages can render successfully for webinars

---
+++ spec/system/topic_page_spec.rb
@@ -0,0 +1,12 @@
+describe "Discourse Zoom | Topic Page", type: :system do
+  fab!(:topic)
+  fab!(:post) { Fabricate(:post, topic: topic) }
+  fab!(:webinar) { Webinar.create(topic: topic, zoom_id: "123") }
+
+  before { SiteSetting.zoom_enabled = true }
+
+  it "renders successfully" do
+    visit "/t/#{topic.slug}/#{topic.id}"
+    expect(page).to have_css(".webinar-banner")
+  end
+end
