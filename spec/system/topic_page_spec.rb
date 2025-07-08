573992d195736366c5f883b46842af1fafac36d6
Sérgio Saquetim <saquetim@discourse.org>
2025-07-08 18:09:10 -0300

    DEV: Add `glimmer_post_stream_mode` contexts to Zoom topic page spec
    Refactor tests in `topic_page_spec.rb` to include cases for `glimmer_post_stream_mode` being enabled and disabled. Improves test coverage by ensuring compatibility under different site settings.

--- spec/system/topic_page_spec.rb
+++ spec/system/topic_page_spec.rb
@@ -1,14 +1,20 @@
 # frozen_string_literal: true
 
 describe "Discourse Zoom | Topic Page", type: :system do
-  fab!(:topic) { Fabricate(:topic_with_op) }
-  fab!(:webinar) { Webinar.create(topic:, zoom_id: "123") }
+  %w[enabled disabled].each do |value|
+    before { SiteSetting.glimmer_post_stream_mode = value }
 
-  before { SiteSetting.zoom_enabled = true }
+    context "when glimmer_post_stream_mode=#{value}" do
+      fab!(:topic) { Fabricate(:topic_with_op) }
+      fab!(:webinar) { Webinar.create(topic:, zoom_id: "123") }
 
-  it "renders successfully" do
-    visit "/t/#{topic.slug}/#{topic.id}"
-    expect(page).to have_css(".webinar-banner")
-    expect(page).to have_css("body.has-webinar")
+      before { SiteSetting.zoom_enabled = true }
+
+      it "renders successfully" do
+        visit "/t/#{topic.slug}/#{topic.id}"
+        expect(page).to have_css(".webinar-banner")
+        expect(page).to have_css("body.has-webinar")
+      end
+    end
   end
 end
